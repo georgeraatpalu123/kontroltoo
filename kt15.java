bf9ddce804193d25a14ad75cb97d7267e2a134b2
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2023-10-23 16:45:28 +0300

    Update kt15.java

--- kt15.java
+++ kt15.java
@@ -3,16 +3,25 @@ import java.util.Scanner;
 public class kt15 {
     public static void main(String[] args) {
         Scanner sisend = new Scanner(System.in);
-        System.out.print("sisesta isikukood: ");
+        System.out.print("Sisesta isikukood: ");
         String isikukood = sisend.nextLine();
         sisend.close();
-        
-        // analuusib isikukoodi
+
+        //try and catch 
+        try {
+            analüüsiIsikukood(isikukood);
+        } catch (NumberFormatException e) {
+            System.out.println("vale");
+        } catch (IllegalArgumentException e) {
+            System.out.println("vale");
+        }
+    }
+        //arvutused
+    public static void analüüsiIsikukood(String isikukood) {
         int sugu = Integer.parseInt(isikukood.substring(0, 1)) % 2 == 0 ? 2 : 1;
         String sünniaeg = isikukood.substring(1, 7);
-       
-        //kuvab soo ja synniaja
-        System.out.println(" sugu" + (sugu == 1 ? "mees" : "naine"));
-        System.out.println(" sünniaeg: " + sünniaeg);
+            // kuvab isikukoodid ja sunniajad
+        System.out.println("Sugu: " + (sugu == 1 ? "mees" : "naine"));
+        System.out.println("Sünniaeg: " + sünniaeg);
     }
 }
