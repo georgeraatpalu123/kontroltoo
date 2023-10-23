9e31b18f00d8fb5b6306e519775a2573ba957c68
georgeraatpalu123 <115462575+georgeraatpalu123@users.noreply.github.com>
2023-10-23 16:45:11 +0300

    Update kt5.java

--- kt5.java
+++ kt5.java
@@ -3,20 +3,23 @@ import java.util.Scanner;
 
 public class kt5{
     public static void main(String[] args) {
-    Scanner sisend = new Scanner(System.in);
+    Scanner sisend = new Scanner(System.in); 
     System.out.print("mitu takistit soovite ühendada: ");
+    
+    //teeb  taisarvuks
     int takistiteArv = sisend.nextInt();
     double kogutakistusJ = 0;
     double kogutakistusRoppselt = 0;
-        
+    //arvutab kokku 
     for (int i = 1; i <= takistiteArv; i++) {
     System.out.print("sisesta" + i + ". takisti takistus ohmides: ");
     double takistus = sisend.nextDouble();
-        
+    
+        // lisab jadamid takistusse
     kogutakistusJ += takistus;
-        kogutakistusRoppselt = 1 / (1 / kogutakistusRoppselt + 1 / takistus);
+    kogutakistusRoppselt = 1 / (1 / kogutakistusRoppselt + 1 / takistus);
     }
-        
+        //kuvab valja arvutused
         System.out.println("jadamisi ühendatud takistite kogutakistus: " + kogutakistusJ + " ohm");
         System.out.println("rööpselt ühendatud takistite kogutakistus: " + kogutakistusRoppselt + " ohm");
         
@@ -25,4 +28,3 @@ public class kt5{
 }
 
     
-
